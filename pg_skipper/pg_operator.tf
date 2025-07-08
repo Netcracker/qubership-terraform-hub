5dbb144bfa76f731cd59266392baeb6f8d34495e
Nikolai Maksak <nikolai.maksak@netcracker.com>
2025-07-08 12:44:30 +0300

    [TEST] Terraform

--- pg_skipper/pg_operator.tf
+++ pg_skipper/pg_operator.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "pg_operator" {
   name       = "patroni-core"
-  namespace  = "${{ vars.NAMESPACE }}"
+  namespace  = "pg-test-1"
   chart      = "../pgskipper-operator/charts/patroni-core"
   timeout    = 1200
   values     = ["../pgskipper-operator/charts/patroni-core/values.yaml"]
