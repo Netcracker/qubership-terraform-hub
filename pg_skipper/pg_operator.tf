2eceeb550ff192871794f4c1be42622d3b4d59b5
Nikolai Maksak <nikolai.maksak@netcracker.com>
2025-07-08 17:30:35 +0300

    [TEST] Terraform

--- pg_skipper/pg_operator.tf
+++ pg_skipper/pg_operator.tf
@@ -5,7 +5,7 @@ variable "PG_INSTALL_NAMESPACE" {
 resource "helm_release" "pg_operator" {
   name       = "patroni-core"
   create_namespace = true
-  namespace = var.PG_INSTALL_NAMESPACE || pg-not-set-ns
+  namespace = var.PG_INSTALL_NAMESPACE
   chart      = "../pgskipper-operator/charts/patroni-core"
   timeout    = 1200
   #values     = ["../../pgskipper-operator/charts/patroni-core/values.yaml"]
