d9f56af67bf0fb71dc845bab6bb8c6a5d40f31a4
Roman Barmin <roman.barmin.qubership@gmail.com>
2025-08-06 16:55:47 +0300

    [feat] ADD EBS installation and roles

--- kubernetes-terraform/main.tf
+++ kubernetes-terraform/main.tf
@@ -81,6 +81,7 @@ module "eks" {
 
     endpoint_public_access = true
     enable_cluster_creator_admin_permissions = true
+    create_cloudwatch_log_group = false
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
