02b033c88665f603891f133b3bbf12002a1c5043
Roman Barmin <roman.barmin.qubership@gmail.com>
2025-09-12 15:15:52 +0300

    Update stop.tf
    chore: delete no longer present github runner from scheduled shutdown

--- ec2-scheduled/stop/stop.tf
+++ ec2-scheduled/stop/stop.tf
@@ -1,9 +1,5 @@
-resource "aws_ec2_instance_state" "stop_github_runner" {
-  instance_id = var.GITHUB_RUNNER_ID
-  state       = "stopped"
-}
-
 resource "aws_ec2_instance_state" "stop_opensearch_runner" {
   instance_id = var.OPENSEARCH_RUNNER_ID
   state       = "stopped"
+
 }
